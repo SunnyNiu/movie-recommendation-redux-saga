28153540c5d94dd4f2b73606c61c423da4269d50
Sunny Niu <sunnyyingli@gmail.com>
2020-04-02 14:19:04 +1300

    fix get movie cases in server

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -1,19 +1,18 @@
-const request = require('supertest');
+import request from 'supertest';
+import server from './server';
+import { getRandomMovie } from './db/db';
 
-const server = require('./server');
-
-const mockGetMovie = { id: 2, name: 'Test2', image: './image1.png' };
-
-jest.mock('./db/db', () => ({
-  getRandomMovie: () => Promise.resolve(mockGetMovie),
-}));
+jest.mock('./db/db');
+jest.mock('request-promise-native');
 
 describe('get random movie', () => {
   it('GET /movie', () => {
+    const data = { id: 2, name: 'Test2', image: './image1.png' };
+    getRandomMovie.mockReturnValueOnce(Promise.resolve(data));
     return request(server)
       .get('/movie')
       .then(res => {
-        expect(res.body).toEqual(mockGetMovie);
+        expect(res.body).toEqual(data);
       });
   });
 });
